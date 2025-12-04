f81f8bfc79007b81307f5a61137f9352c7263fb5
Yaqut <yaguter-bpa202@code.edu.az>
2025-12-04 12:35:15 +0400

    localstorage added

--- app.js
+++ app.js
@@ -1,66 +1,90 @@
-const myInput=document.getElementById("text")
-const myCreate=document.getElementById("btn")
-const myRead=document.getElementById("read")
-const myUl=document.getElementById("list")
+let myInput=document.getElementById('text')
+let myBtn=document.getElementById('btn')
+let read=document.getElementById('read')
+let myUl=document.getElementById('ul')
 
-myCreate.addEventListener('click', ()=>{
-    if(myInput.value==''){
-        alert("Write something to enter")
+const items=document.getElementsByClassName('item')
+let editIndex=null;
+function getTodos(){
+    return JSON.parse(localStorage.getItem("todos") || "[]")
+}
+
+myBtn.addEventListener('click', ()=>{
+    if(!myInput.value){
+        alert("Write something to add")
     }
     else{
-        const myLi=document.createElement('li')
+            if(myBtn.textContent=="Save"){
+                let todos=getTodos()
+                todos[editIndex]=myInput.value
+                localStorage.setItem("todos", JSON.stringify(todos))
+
+                myBtn.textContent="Create Todo"
+                myInput.value=""
+                editIndex=null
+                showElements();
+                return;
+
+            }
+
+
+        // const myLi=document.createElement('li')
+        // myLi.className="item"
+        // myLi.textContent=myInput.value
+        // myUl.append(myLi)
+        // let check=document.createElement('p')
+        // check.textContent='☑️'
+        // let edit=document.createElement('p')
+        // edit.textContent='🖊️'
+        // let del=document.createElement('p')
+        // del.textContent='🗑️'
+        // myLi.append(check, edit, del)
+        // myLi.style.display="flex"
+        // myLi.style.alignItems="center"
+
+        let todos=getTodos();
+        todos.push(myInput.value)
+        localStorage.setItem("todos", JSON.stringify(todos))
+    
+        myInput.value = ""  
+        showElements()
+
+
+    }
+})
+
+function showElements(){
+
+    let todos =getTodos()
+    myUl.innerHTML = ""; 
+    todos.forEach(todo => {
+        let myLi=document.createElement('li')
         myLi.className="item"
-    
+        myLi.textContent=todo;
 
-        
-const divİcon=document.createElement('div')
-divİcon.className="icons"
-const check=document.createElement('i')
-check.className="fa-solid fa-circle-check"
-
-const edit=document.createElement('i')
-edit.className="fa-solid fa-pen-to-square"
-
-const del=document.createElement('i')
-del.className="fa-solid fa-trash-can"
-        myLi.textContent=myInput.value
+        let check=document.createElement('p')
+        check.textContent = '☑️';
+        let edit=document.createElement('p')
+        edit.textContent = '🖊️';
+        let del=document.createElement('p')
+        del.textContent = '🗑️';
+        myLi.append(check,edit,del)
+        myLi.style.display="flex"
+        myLi.style.alignItems='center'
         myUl.append(myLi)
+        del.addEventListener('click', ()=>{
+            myLi.remove()
+        })
 
-myLi.append(check,edit,del)
-myLi.style.display='flex'
-myLi.style.gap='10px'
-myInput.value=''
+        del.addEventListener('click', ()=>{
+            let todos = getTodos();
+            todos.splice(index, 1); 
+            localStorage.setItem("todos", JSON.stringify(todos));
+            showElements(); 
+        })
+            myUl.append(myLi)
+    });
 
-
-
-del.addEventListener('click', ()=>{
-myLi.remove()
-})
-
-check.addEventListener('click',()=>{
-    myLi.style.color='green'
-    myLi.style.textDecoration='line-through'
-})
-
-edit.addEventListener('click', ()=>{
+}
+window.onload=showElements;
     
-    myInput.value=myLi.textContent
-    myCreate.textContent="Save"
-    isEditing=true;
-    
-})
-
-myCreate.addEventListener('click', ()=>{
-    if(!isEditing){
-        myLi.textContent=myInput.value
-    }
-    else{
-        myLi.textContent = myInput.value;
-        myCreate.textContent = "Add Todo"; 
-        isEditing = false;
-    }
-    myInput.value=''
-})
-
-    }
-})
